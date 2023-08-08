bff3f791551d28a5a90f48d488add708fc4c06e3
Judge-Paul Ogebe <110723341+Judge-Paul@users.noreply.github.com>
2023-08-08 11:28:05 +0100

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,9 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
     <main>
-      <form className="mx-auto text-center bg-blue-600 w-[400px] h-[800px]">
-        <input type="text" className='mt-5' />
-        <button className="block bg-green-500 hover:bg-green-700">
-          Submit
-        </button>
-      </form>
+      <h1>Abeg do me 2k</h1>
+      <p>E don red</p>
+      <p>2000459831 Kuda Bank</p>
     </main>
   )
 }
