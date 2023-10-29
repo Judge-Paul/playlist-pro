c7cbb8a9f1fd43022179d91b22c7cf8fe8c750e4
Judge-Paul <judgepaulogebe3@gmail.com>
2023-10-29 14:32:02 +0100

    feat: add next-themes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,21 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import { ThemeProvider } from "@/components/theme-provider";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
-        <Main />
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Main />
+        </ThemeProvider>
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
