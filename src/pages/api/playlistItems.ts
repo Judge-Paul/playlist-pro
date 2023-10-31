a4f1e86848e5c9686d93ac378a063e74880e2add
Judge-Paul <judgepaulogebe3@gmail.com>
2023-10-31 12:05:18 +0100

    feat: get playlist id from api request

--- src/pages/api/playlistItems.ts
+++ src/pages/api/playlistItems.ts
@@ -3,29 +3,32 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
 
 type Data = {
-  error: any;
+  message?: any;
+  error?: any;
 };
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  const apiKey = "";
-  const playlistId = "PLqkLaKB2GJhWXV9rcarwvn06ISlL_9mPQ&si=lvHKuJAnxBsIAlVp";
+  const apiKey = process.env.API_KEY;
+  // const playlistId = "PLqkLaKB2GJhWXV9rcarwvn06ISlL_9mPQ&si=lvHKuJAnxBsIAlVp";
 
-  // const { playlistId } = req.query;
+  const { id } = req.query;
 
-  if (!playlistId) {
+  if (!id) {
     return res.status(400).json({ error: "Missing playlistId" });
   }
 
   try {
     // Make a request to the YouTube Data API
-
     const response = await axios.get(
-      `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&id=${playlistId}&key=${apiKey}`,
+      `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${id}&key=${apiKey}`,
     );
     res.status(200).json(response.data);
+    if (response.data.items.length === 0) {
+      return res.status(400).json({ error: "Playlist Not Found" });
+    }
   } catch (error) {
     res.status(500).json({ error: "Failed to fetch data" });
   }
