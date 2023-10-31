61f54480a1c8498f4e3e1f584e37dd5a5aef4a25
Judge-Paul <judgepaulogebe3@gmail.com>
2023-10-31 23:49:34 +0100

    feat: return an array of video items from server

--- src/pages/api/playlistItems.ts
+++ src/pages/api/playlistItems.ts
@@ -1,19 +1,11 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
 
-type Data = {
-  message?: any;
-  error?: any;
-};
-
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>,
+  res: NextApiResponse,
 ) {
   const apiKey = process.env.API_KEY;
-  // const playlistId = "PLqkLaKB2GJhWXV9rcarwvn06ISlL_9mPQ&si=lvHKuJAnxBsIAlVp";
-
   const { id } = req.query;
 
   if (!id) {
@@ -21,14 +13,35 @@ export default async function handler(
   }
 
   try {
-    // Make a request to the YouTube Data API
-    const response = await axios.get(
-      `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${id}&key=${apiKey}`,
-    );
-    res.status(200).json(response.data);
-    if (response.data.items.length === 0) {
+    // Function to make requests recursively and accumulate items
+    const fetchItems = async (pageToken = "") => {
+      const response = await axios.get(
+        `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${id}&key=${apiKey}&pageToken=${pageToken}`,
+      );
+
+      const { items, nextPageToken } = response.data;
+
+      if (!items || items.length === 0) {
+        return [];
+      }
+
+      const itemsArray = items;
+
+      if (nextPageToken) {
+        const nextItems = await fetchItems(nextPageToken);
+        itemsArray.push(...nextItems);
+      }
+
+      return itemsArray;
+    };
+
+    const allItems = await fetchItems();
+
+    if (allItems.length === 0) {
       return res.status(400).json({ error: "Playlist Not Found" });
     }
+
+    res.status(200).json(allItems);
   } catch (error) {
     res.status(500).json({ error: "Failed to fetch data" });
   }
