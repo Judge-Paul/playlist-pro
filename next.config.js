e0481bca7bc36ebc28f10ec651d3c6c27dc6bde2
Judge-Paul <judgepaulogebe3@gmail.com>
2023-10-31 23:47:59 +0100

    chore: configure yt img host in project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+        port: "",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
