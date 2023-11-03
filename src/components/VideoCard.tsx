18b6706d2d4ced5260eeaaa7cbc4eff2b928cb9b
Judge-Paul <judgepaulogebe3@gmail.com>
2023-11-03 01:30:11 +0100

    fix: add default values if data isn't available

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -22,15 +22,18 @@ export default function VideoCard({
   return (
     <div className="mb-2 justify-between gap-3 border border-secondary p-5 sm:flex">
       <Image
-        src={thumbnails.default.url}
-        width={thumbnails.default.width}
-        height={thumbnails.default.height}
+        src={
+          thumbnails?.default?.url ||
+          "https://www.gravatar.com/avatar/18c42a6912b0288d4c6a5c0ec3e3553d?s=120&d=blank&r=g"
+        }
+        width={thumbnails?.default?.width || 160}
+        height={thumbnails?.default?.height || 160}
         className="h-40 w-full bg-secondary sm:h-28 sm:w-40"
-        alt="Example Image"
+        alt={title || "Image not Available"}
       />
       <div className="mt-3 sm:mt-0 sm:w-3/4">
         <h4 className="line-clamp-2 h-12 w-full font-semibold sm:w-2/3 md:w-1/2">
-          {title}
+          {title || "N/A"}
         </h4>
         <p className="mt-4 line-clamp-3 h-12 w-full text-xs md:w-2/3">
           {description || "N/A"}
@@ -38,7 +41,7 @@ export default function VideoCard({
       </div>
       <div className="mt-3 flex justify-between sm:flex-col">
         {data ? (
-          <Link href={data[2].link}>
+          <Link href={data[0]?.link || ""}>
             <Download className="h-8 w-8 cursor-pointer hover:scale-[.90] active:scale-[.85]" />
           </Link>
         ) : (
