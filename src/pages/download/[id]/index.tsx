3316541a08bfa74a84bf4269a5e1a21ea2f962d5
Judge-Paul <judgepaulogebe3@gmail.com>
2023-12-06 00:15:23 +0100

    feat: create option to select quality

--- src/pages/download/[id]/index.tsx
+++ src/pages/download/[id]/index.tsx
@@ -22,13 +22,19 @@ export default function Download() {
   if (data?.error) {
     toast.error(data.error + ". Reload the page to Try Again.");
   }
+
+  function changeQuality(quality: string) {
+    quality = quality.toLowerCase();
+    router.push(`/download/${id}?quality=${quality}`);
+  }
+
   return (
     <main>
       <h1 className="mt-10 text-center text-3xl font-bold lg:text-5xl">
         Playlist Videos
       </h1>
       <div className="mx-auto mt-7 max-w-5xl px-8">
-        <div className="flex justify-between">
+        <div className="justify-between sm:flex">
           {data && !data.error ? (
             <h4 className="my-auto text-lg">
               {data.length} Videos in Playlist
@@ -36,15 +42,42 @@ export default function Download() {
           ) : (
             <div className="my-auto h-6 w-40 bg-secondary"></div>
           )}
-          <Popover>
-            <PopoverTrigger className="flex rounded-md border border-primary px-4 py-2 dark:border-secondary">
-              Download All <ChevronDown className="ml-2" />
-            </PopoverTrigger>
-            <PopoverContent className="max-w-max">
-              <Button className="block">Download .zip</Button>
-              <Button className="mt-2 block">Download .rar</Button>
-            </PopoverContent>
-          </Popover>
+          <div className="sm:flex sm:gap-2">
+            <Popover>
+              <PopoverTrigger className="ml-auto mt-2 flex rounded-md border border-primary px-4 py-2 dark:border-secondary sm:mt-0">
+                Quality <ChevronDown className="ml-2" />
+              </PopoverTrigger>
+              <PopoverContent className="max-w-max">
+                <Button
+                  onClick={() => changeQuality("high")}
+                  className="block w-full"
+                >
+                  High
+                </Button>
+                <Button
+                  onClick={() => changeQuality("medium")}
+                  className="mt-2 block w-full"
+                >
+                  Medium
+                </Button>
+                <Button
+                  onClick={() => changeQuality("low")}
+                  className="mt-2 block w-full"
+                >
+                  Low
+                </Button>
+              </PopoverContent>
+            </Popover>
+            <Popover>
+              <PopoverTrigger className="ml-auto mt-2 flex rounded-md border border-primary px-4 py-2 dark:border-secondary sm:mt-0">
+                Download All <ChevronDown className="ml-2" />
+              </PopoverTrigger>
+              <PopoverContent className="max-w-max">
+                <Button className="block">Download .zip</Button>
+                <Button className="mt-2 block">Download .rar</Button>
+              </PopoverContent>
+            </Popover>
+          </div>
         </div>
         <div className="mt-7">
           {data && !data?.error ? (
