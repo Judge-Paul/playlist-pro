99b2973cfb762f378de610d2ee45e515a2622cc1
Judge-Paul <judgepaulogebe3@gmail.com>
2023-12-06 00:05:19 +0100

    feat: create function to format bytes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -19,3 +19,15 @@ export function getPlaylistId(url: string) {
 }
 
 export const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+export function formatBytes(bytes: number, decimals = 2) {
+  if (bytes === 0) return "0 Bytes";
+
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ["Bytes", "KB", "MB"];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
+}
