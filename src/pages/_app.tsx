7dbf024d52aa60d3f07b9685e444c2ecb2a34879
Judge-Paul <judgepaulogebe3@gmail.com>
2023-12-25 19:51:03 +0100

    chore: add title and favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Toaster } from "sonner";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Playlist Pro</title>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
       <Toaster richColors position="top-right" />
       <Component {...pageProps} />
     </>
