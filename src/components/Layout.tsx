c97dbf3cd40085552d428c604b9491f0699349a8
Judge-Paul <judgepaulogebe3@gmail.com>
2023-12-25 19:51:30 +0100

    chore: add types to layout file

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,17 +1,23 @@
+import React, { ReactNode } from "react";
 import Head from "next/head";
 import Navbar from "./Navbar";
 
-export default function Layout({ children }) {
+type LayoutProps = {
+  children: ReactNode;
+};
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <>
       <Head>
         <title>YTPlaylistPro</title>
         <meta name="description" content="My App Description" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
       {children}
       <footer>This is the footer</footer>
     </>
   );
-}
+};
+
+export default Layout;
