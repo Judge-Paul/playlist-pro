fd885976f444a3120edb387138c7895a57ef86d6
Judge-Paul <judgepaulogebe3@gmail.com>
2023-12-25 11:10:55 +0100

    refactor: remove regex from playlist id function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,17 +5,10 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function getPlaylistId(url: string) {
-  // Regular expression to match a YouTube playlist URL
-  const playlistRegex = /list=([A-Za-z0-9_-]+)/;
-
-  // Use the regex to find and extract the playlist ID
-  const match = url.match(playlistRegex);
-
-  if (match && match[1]) {
-    return match[1];
-  }
-  return "";
+export function getPlaylistId(url: string): string {
+  const parsedURL = new URL(url);
+  const playlistId = parsedURL.searchParams.get("list") ?? "";
+  return playlistId;
 }
 
 export const fetcher = (url: string) => fetch(url).then((res) => res.json());
