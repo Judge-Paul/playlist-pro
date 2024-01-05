755fc39b1ec5875ac3d29ffdab4cd15455767fc8
Judge-Paul <judgepaulogebe3@gmail.com>
2024-01-05 07:00:23 +0100

    fix: prevent components from running on the server

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,27 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Toaster } from "sonner";
 import Head from "next/head";
+import { ThemeProvider } from "@/components/theme-provider";
+import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <title>Playlist Pro</title>
-        <link rel="icon" href="/favicon.png" />
-      </Head>
-      <Toaster richColors position="top-right" />
-      <Component {...pageProps} />
+      <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+        <Layout>
+          <Head>
+            <title>Playlist Pro</title>
+            <link rel="icon" href="/favicon.png" />
+          </Head>
+          <Toaster richColors position="top-right" />
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
     </>
   );
 }
