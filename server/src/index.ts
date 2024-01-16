034ea42e2e34cf15691238e54aa99e63aa4a1051
Judge-Paul <judgepaulogebe3@gmail.com>
2024-01-16 23:58:58 +0100

    feat: create get playlist items endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,20 @@
 import express, { Request, Response } from "express";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import JSZip from "jszip";
 import dotenv from "dotenv";
 import cors from "cors";
 import morgan from "morgan";
+import { PlaylistItem } from "./@types";
 
 dotenv.config();
 
 const app = express();
 const port = parseInt(process.env.PORT ?? "") || 8080;
+const clientURL = process.env.CLIENT_URL;
 
 app.use(morgan("dev"));
-app.use(cors({ origin: "*" }));
 app.use(express.json({ limit: "250kb" }));
+app.use(cors({ origin: "*" }));
 
 app.get("/", (req: Request, res: Response) => {
   res.send(`Go to /createZip to get ZIP file`);
@@ -53,6 +55,61 @@ app.get("/createZip", async (req: Request, res: Response) => {
     zip.generateNodeStream({ streamFiles: true }).pipe(res);
   } catch (error) {
     return res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.get("/playlistItems", async (req: Request, res: Response) => {
+  res.setHeader("CDN-Cache-Control", "max-age=21600");
+  res.setHeader(
+    "Cache-Control",
+    "max-age=18000, must-revalidate, stale-while-revalidate=21600",
+  );
+  const apiKey = process.env.GOOGLE_API_KEY;
+  const { id } = req.query;
+
+  if (!id) {
+    return res.status(400).json({ error: "Missing playlistId" });
+  }
+
+  try {
+    const response = await axios.get(
+      `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=${id}&key=${apiKey}`,
+    );
+
+    if (response.status === 200) {
+      const items: PlaylistItem[] = response.data.items;
+
+      if (!items || items.length === 0) {
+        return res.status(404).json({ error: "Playlist Items Not Found" });
+      }
+
+      const videoIds = items.map((item) => item.snippet.resourceId.videoId);
+
+      const downloadLinks = await axios.get(
+        `${clientURL}/api/downloadLinks?videoIds?${encodeURIComponent(
+          JSON.stringify(videoIds),
+        )}`,
+      );
+
+      for (let i = 0; i < items.length; i++) {
+        items[i].downloadLinks = downloadLinks.data[i].downloadLinks;
+      }
+      return res.status(200).json(items);
+    } else {
+      return res.status(500).json({ error: "Failed getting playlists data" });
+    }
+  } catch (error: any) {
+    if (axios.isAxiosError(error)) {
+      const axiosError: AxiosError = error;
+
+      if (axiosError.response?.status === 404) {
+        return res.status(404).json({ error: "Playlist not found" });
+      } else {
+        return res.status(500).json({ error });
+      }
+    } else {
+      return res.status(500).json({ error: "Unexpected error occurred" });
+    }
   }
 });
 
