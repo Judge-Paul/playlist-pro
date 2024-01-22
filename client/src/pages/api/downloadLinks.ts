5b411f317dc9076ac51d2bd23d6aafe450232c72
Judge-Paul <judgepaulogebe3@gmail.com>
2024-01-22 14:32:14 +0100

    feat: get all download links at once

--- client/src/pages/api/downloadLinks.ts
+++ client/src/pages/api/downloadLinks.ts
@@ -2,66 +2,80 @@ import { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
 import * as cheerio from "cheerio";
 
+interface DownloadLinks {
+  [quality: string]: {
+    resolution: string;
+    format: string;
+    link: string;
+    size: number;
+  };
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  res.setHeader("CDN-Cache-Control", "max-age=2419200");
-  // res.setHeader("Cache-Control", "max-age=3600");
-  const { videoId } = req.query;
+  const videoIds = JSON.parse(req.query.videoIds as string);
 
-  if (!videoId) {
-    return res.status(400).json({ error: "Missing videoId" });
+  if (!videoIds) {
+    return res.status(400).json({ error: "Missing videoIds" });
   }
 
   try {
-    const response = await axios.get(
-      `https://ytsnag.com/download?v=https://www.youtube.com/watch?v=${videoId}`,
+    const responses = await Promise.all(
+      videoIds.map(async (videoId: string) => {
+        try {
+          const response = await axios.get(
+            `https://ytsnag.com/download?v=https://www.youtube.com/watch?v=${videoId}`,
+          );
+
+          if (response.status === 200) {
+            const $ = cheerio.load(response.data);
+
+            const qualityMap = ["high", "medium", "low"];
+            const firstTable = $(".downloadsTable").first();
+            const rows = firstTable.find("tr:gt(0)");
+
+            const promises = rows
+              .map(async (i, row) => {
+                const quality = qualityMap[i];
+                const resolution = $(row).find("td:nth-child(1)").text();
+                const format = $(row).find("td:nth-child(2)").text();
+                const link = $(row).find("a.downloadBtn").attr("href");
+
+                const headResponse = await axios.head(link ?? "");
+                const size =
+                  parseInt(headResponse.headers["content-length"]) ?? 0;
+
+                return {
+                  [quality]: {
+                    resolution,
+                    format,
+                    link,
+                    size,
+                  },
+                };
+              })
+              .get();
+
+            const results = await Promise.all(promises);
+            let downloadLinks: DownloadLinks = {};
+
+            results.forEach(async (result) => {
+              Object.assign(downloadLinks, result);
+            });
+
+            return downloadLinks;
+          } else {
+            throw new Error("Failed getting download links");
+          }
+        } catch (error) {
+          throw new Error("Internal server error");
+        }
+      }),
     );
 
-    if (response.status === 200) {
-      const $ = cheerio.load(response.data);
-
-      const qualityMap = ["high", "medium", "low"];
-      const firstTable = $(".downloadsTable").first();
-      const rows = firstTable.find("tr:gt(0)");
-
-      const promises = rows
-        .map(async (i, row) => {
-          const quality = qualityMap[i];
-          const resolution = $(row).find("td:nth-child(1)").text();
-          const format = $(row).find("td:nth-child(2)").text();
-          const link = $(row).find("a.downloadBtn").attr("href");
-
-          const headResponse = await axios.head(link ?? "");
-          const size = parseInt(headResponse.headers["content-length"]) ?? 0;
-
-          return {
-            [quality]: {
-              resolution,
-              format,
-              link,
-              size,
-            },
-          };
-        })
-        .get();
-
-      const results = await Promise.all(promises);
-      let downloadLinks = {};
-
-      results.forEach((result) => {
-        Object.assign(downloadLinks, result);
-      });
-
-      if (JSON.stringify(downloadLinks) !== "{}") {
-        res.status(200).json({ downloadLinks });
-      } else {
-        res.status(response.status).json({ error: "Links not available." });
-      }
-    } else {
-      res.status(response.status).json({ error: "Unable to access URL" });
-    }
+    res.status(200).json(responses);
   } catch (error) {
     res.status(500).json({ error: "Internal server error" });
   }
