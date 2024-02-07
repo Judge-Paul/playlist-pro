2dc12209c99aaecc3ef946bacfc87b5f5c06499c
Judge-Paul <judgepaulogebe3@gmail.com>
2024-02-08 00:07:18 +0100

    refactor: cleanup unused endpoints and packages

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,18 @@
 import express, { Request, Response } from "express";
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 import JSZip from "jszip";
 import dotenv from "dotenv";
 import cors from "cors";
 import morgan from "morgan";
-import { PlaylistItem } from "./@types";
-import NodeCache from "node-cache";
 import { getQualities } from "./utils";
 import { Redis } from "@upstash/redis";
+import { PlaylistItem } from "./@types";
 
 dotenv.config();
 
 const app = express();
 const port = parseInt(process.env.PORT ?? "") || 8080;
 const clientURL = process.env.CLIENT_URL;
-const serverURL = process.env.SERVER_URL;
 const redisURL = process.env.REDIS_URL || "";
 const redisToken = process.env.REDIS_TOKEN || "";
 
@@ -28,9 +26,7 @@ app.use(express.json({ limit: "400kb" }));
 app.use(cors({ origin: clientURL }));
 
 app.get("/", (req: Request, res: Response) => {
-  res.send(
-    `Go to /createZip to get ZIP file\nGo to /playlistItems to get playlist items`,
-  );
+  res.send(`Go to /createZip to get ZIP file`);
 });
 
 app.get("/createZip", async (req: Request, res: Response) => {
@@ -44,7 +40,7 @@ app.get("/createZip", async (req: Request, res: Response) => {
     const zip = new JSZip();
 
     await Promise.all(
-      playlist.map(async (item: any) => {
+      playlist.map(async (item: PlaylistItem) => {
         const qualities = getQualities(playlist);
         const quality = qualities[0];
         const downloadLink = item.downloadLinks[quality]?.link;
@@ -63,74 +59,6 @@ app.get("/createZip", async (req: Request, res: Response) => {
     zip.generateNodeStream({ streamFiles: true }).pipe(res);
   } catch (error) {
     return res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-const cacheDuration = 6 * 60 * 60;
-
-const playlistCache = new NodeCache({
-  stdTTL: cacheDuration,
-  checkperiod: cacheDuration * 0.2,
-});
-
-app.get("/playlistItems", async (req: Request, res: Response) => {
-  const apiKey = process.env.GOOGLE_API_KEY;
-  const id = req.query.id as string;
-
-  if (!id) {
-    return res.status(400).json({ error: "Missing playlistId" });
-  }
-
-  // Check if the data is in the cache
-  const cachedData = playlistCache.get<PlaylistItem[]>(id);
-
-  if (cachedData) {
-    console.log(`Sending cached data for playlistId: ${id}`);
-    return res.status(200).json(cachedData);
-  }
-
-  try {
-    const response = await axios.get(
-      `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=${id}&key=${apiKey}`,
-    );
-
-    if (response.status === 200) {
-      const items: PlaylistItem[] = response.data.items;
-
-      if (!items || items.length === 0) {
-        return res.status(404).json({ error: "Playlist Items Not Found" });
-      }
-
-      const videoIds = items.map((item) => item.snippet.resourceId.videoId);
-
-      const linksRes = await axios.get(
-        `${clientURL}/api/downloadLinks?videoIds=${encodeURIComponent(
-          JSON.stringify(videoIds),
-        )}`,
-      );
-      items.map((item: PlaylistItem, index: number) => {
-        item.downloadLinks = linksRes.data[index];
-        item.qualities = Object.getOwnPropertyNames(item.downloadLinks);
-      });
-      // Cache the data for the specified duration
-      playlistCache.set(id, items);
-
-      return res.status(200).json(items);
-    } else {
-      return res.status(500).json({ error: "Failed getting playlists data" });
-    }
-  } catch (error: any) {
-    if (axios.isAxiosError(error)) {
-      const axiosError: AxiosError = error;
-
-      if (axiosError.response?.status === 404) {
-        return res.status(404).json({ error: "Playlist not found" });
-      } else {
-        return res.status(500).json({ error });
-      }
-    } else {
-      return res.status(500).json({ error: "Unexpected error occurred" });
-    }
   }
 });
 
